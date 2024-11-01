5986157c20b3653940c90ac16b651e57d0e7fb1d
Theosov <nesterovila14@gmail.com>
2024-11-01 22:56:00 +0200

    Add drag (#20)
    Co-authored-by: Theosov <theosov@tutanota.com>

--- src/components/Rectangle/Rectangle.tsx
+++ src/components/Rectangle/Rectangle.tsx
@@ -1,5 +1,7 @@
-import { useEffect } from 'react';
-import { useAddToRenderQueue, useRenderer } from '../../context/CanvasContext';
+import { useEffect, useRef } from 'react';
+import { useRenderer } from '../../context/CanvasContext';
+import { DragE, Shape } from '../../types';
+import { UUID } from '../../utils/uuid';
 
 interface RectangleProps {
     x: number;
@@ -7,21 +9,43 @@ interface RectangleProps {
     width: number;
     height: number;
     color: [number, number, number, number];
+    isDraggable?: boolean;
+    onDragStart?: (e: DragE) => void;
+    onDrag?: (e: DragE) => void;
+    onDragEnd?: (e: DragE) => void;
 }
 
 export function Rectangle(props: RectangleProps): null {
   const { x, y, width, height, color } = props;
   const renderer = useRenderer();
-  const addToRenderQueue = useAddToRenderQueue();
+  const shapeId = useRef(UUID());
 
   useEffect(() => {
-    if (renderer && addToRenderQueue) {
+    if (renderer) {
       const renderFn = (): void => {
         renderer.drawRectangle(x, y, width, height, color);
       };
-      addToRenderQueue(renderFn);
-      renderer.addToRenderQueue(renderFn);
+
+      const shape: Shape = {
+        props: {
+          isDraggable: props.isDraggable === true,
+          onDragStart: props.onDragStart,
+          onDrag: props.onDrag,
+          onDragEnd: props.onDragEnd,
+        },
+        render: renderFn,
+        hitTest: (x: number, y: number) => {
+          return x >= props.x && 
+                 x <= props.x + props.width &&
+                 y >= props.y && 
+                 y <= props.y + props.height;
+        },
+      };
+      renderer.addToRenderQueue(shape, shapeId.current);
       renderer.render();
+
+      return (): void => {
+      };
     }
   }, [x, y, width, height, color, renderer]);
 
